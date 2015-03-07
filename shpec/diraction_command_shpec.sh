a2adc0d8bf2154bcf2531657e8fd5104b373f074
Adriean Khisbe <adriean.khisbe@live.fr>
2015-03-07 10:56:01 +0100

    tests: add draft of files

---
+++ shpec/diraction_command_shpec.sh
@@ -0,0 +1,11 @@
+# Spec for the diraction command
+# -*- mode: ruby  -*-
+
+describe "Diraction Command"
+   it "Transfer existing commands"
+      # §TODO
+   end
+   it "Error for non existing commands"
+      # §TODO
+   end
+end
---
+++ shpec/diraction_dispatcher_shpec.sh
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Spec for the dispatcher commands
+# -*- mode: ruby  -*-
+
+describe "Dispacher Command"
+   it "Simple cd works"
+      # §TODO
+   end
+   it "Can execute commands"
+      # §TODO
+   end
+end
