6b6a8cad1763c70c7b0e4feb49829a9f9d6cca8b
Adriean Khisbe <adriean.khisbe@live.fr>
2015-03-07 12:48:01 +0100

    tests: dispatcher spec (require altests shpec)

--- shpec/diraction_command_shpec.sh
+++ shpec/diraction_command_shpec.sh
@@ -1,11 +1,31 @@
 # Spec for the diraction command
-# -*- mode: ruby  -*-
 
-describe "Diraction Command"
-   it "Transfer existing commands"
-      # §TODO
-   end
-   it "Error for non existing commands"
-      # §TODO
-   end
+# PB: rely on funcion, only existing in zsh, but zsh has end...
+
+describe "Diraction Commands"
+
+  before
+     load $__/../diractions.plugin.zsh
+  end
+
+  describe "Dispatcher"
+
+    it "Transfer existing commands"
+        ok=false
+        stub_command diraction-list "ok=true"
+        diraction list
+        unstub_command diraction-list
+        assert equal $ok true
+
+     end
+     it "Error for non existing commands"
+        diraction whatthefuck 2> /dev/null
+        assert equal $? 1
+      end
+     it "Error if no subcommand"
+        diraction 2> /dev/null
+        assert equal $? 1
+     end
+  end
+
 end
