1210a1d273509bafda6d3f935c1ff442063ee1a6
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-02 23:06:38 +0200

    test: try out the dot for loading instead of source

--- shpec/diraction_command_shpec.zsh
+++ shpec/diraction_command_shpec.zsh
@@ -6,7 +6,7 @@ echo $0 $SHELL $ZSH_EVAL_CONTEXT $BASH_SOURCE bashv $BASH_VERSION zshv $ZSH_VERS
 
 setopt aliases
 CURRENT_DIR="$PWD"
-source $(dirname $0:A)/../diractions.plugin.zsh
+. $(dirname $0:A)/../diractions.plugin.zsh
 
 describe "Diraction Commands"
 
