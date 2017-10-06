417843a4655281a3cd813460130d15f9ca3fcaf5
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-06 23:05:38 +0200

    test: add more test for the completion of commands

--- shpec/diraction_dispatcher_completion_shpec.zsh
+++ shpec/diraction_dispatcher_completion_shpec.zsh
@@ -12,6 +12,7 @@ function __diraction-dispatch() {
 DIR_DIR=/tmp/diraction-test
 mkdir $DIR_DIR
 touch ${DIR_DIR}/{a,b,c}
+mkdir -p ${DIR_DIR}/dir{1,2,3}
 diraction create dir $DIR_DIR
 
 completion_commands=(compadd _message _command_names _path_files _directories _ls _describe)
@@ -26,6 +27,25 @@ describe "Dispacher Completion"
     assert grep "$output" "compadd -x Whitelisted commands"
     assert grep "$output" "_describe -t commands Dispatcher subcommand _DIRACTION_HELP_SUBCOMMAND"
     assert grep "$output" "make.*git.*du"
+  end
+  it "Call diraction_dispatch with word arg"
+    export CURRENT=3 words=(__diraction-dispatch $DIR_DIR ls)
+    output="$(__diraction-dispatch)"
+    assert grep "$output" "compadd -x Whitelisted commands"
+    assert grep "$output" "_describe -t commands Dispatcher subcommand _DIRACTION_HELP_SUBCOMMAND"
+    assert grep "$output" "make.*git.*du"
+  end
+
+  it "Call diraction_dispatch with some dir prefix that do not exist"
+    export CURRENT=3 words=(__diraction-dispatch $DIR_DIR /xyz)
+    output="$(__diraction-dispatch)"
+    assert grep "$output" '_message .\?No matching subdir.\?'
+  end
+
+  it "Call diraction_dispatch with some dir prefix that do exist"
+    export CURRENT=3 words=(__diraction-dispatch $DIR_DIR /dir)
+    output="$(__diraction-dispatch)"
+    assert grep "$output" 'compadd -S .\?.\? \?-X .\?Matching Subdirs.\? -U -a paths'
   end
 end
 rm -rf $DIR_DIR
