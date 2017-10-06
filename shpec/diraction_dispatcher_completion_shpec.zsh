b5b8f1fc3f0a4d52cb5a7a8bc64d1ec49895655a
AdrieanKhisbe <adriean.khisbe@live.fr>
2017-10-07 01:11:32 +0200

    test: new test to cover the core logic of the completions

--- shpec/diraction_dispatcher_completion_shpec.zsh
+++ shpec/diraction_dispatcher_completion_shpec.zsh
@@ -15,20 +15,21 @@ touch ${DIR_DIR}/{a,b,c}
 mkdir -p ${DIR_DIR}/dir{1,2,3}
 diraction create dir $DIR_DIR
 
-completion_commands=(compadd _message _command_names _path_files _directories _ls _describe)
+completion_commands=(compadd _message _command_names _path_files _directories _ls _describe _options_set)
 for cmd in $completion_commands; do
     stub_command $cmd "echo $cmd "'$@ '
 done
 
-describe "Dispacher Completion"
-  it "Call diraction_dispatch with no args"
+describe "Dispatcher Completion"
+  it "Completion with no args"
     export CURRENT=3 words=(__diraction-dispatch $DIR_DIR)
     output="$(__diraction-dispatch)"
     assert grep "$output" "compadd -x Whitelisted commands"
     assert grep "$output" "_describe -t commands Dispatcher subcommand _DIRACTION_HELP_SUBCOMMAND"
     assert grep "$output" "make.*git.*du"
   end
-  it "Call diraction_dispatch with word arg"
+
+  it "Completion with word arg"
     export CURRENT=3 words=(__diraction-dispatch $DIR_DIR ls)
     output="$(__diraction-dispatch)"
     assert grep "$output" "compadd -x Whitelisted commands"
@@ -36,16 +37,55 @@ describe "Dispacher Completion"
     assert grep "$output" "make.*git.*du"
   end
 
-  it "Call diraction_dispatch with some dir prefix that do not exist"
+  it "Completion with some dir prefix that do not exist"
     export CURRENT=3 words=(__diraction-dispatch $DIR_DIR /xyz)
     output="$(__diraction-dispatch)"
-    assert grep "$output" '_message .\?No matching subdir.\?'
+    assert grep "$output" "_message .\?No matching subdir.\?"
   end
 
-  it "Call diraction_dispatch with some dir prefix that do exist"
+  it "Completion with some dir prefix that do exist"
     export CURRENT=3 words=(__diraction-dispatch $DIR_DIR /dir)
     output="$(__diraction-dispatch)"
-    assert grep "$output" 'compadd -S .\?.\? \?-X .\?Matching Subdirs.\? -U -a paths'
+    assert grep "$output" "compadd -S .\?.\? \?-X .\?Matching Subdirs.\? -U -a paths"
+  end
+
+  it "Completion with some whitelisted command"
+    export CURRENT=4 words=(__diraction-dispatch $DIR_DIR ls -)
+    output="$(__diraction-dispatch)"
+    assert grep "$output" "_ls"
+  end
+
+  it "Completion with some subdir after slash"
+    export CURRENT=4 words=(__diraction-dispatch $DIR_DIR / dir)
+    output="$(__diraction-dispatch)"
+    assert grep "$output" "_directories -W $DIR_DIR"
+  end
+
+  it "Completion after interactive"
+    export CURRENT=4 words=(__diraction-dispatch $DIR_DIR interactive)
+    output="$(__diraction-dispatch)"
+    assert grep "$output" "_message Just hit enter, and enter the interactive mode :)"
+  end
+
+  describe "Completion of exec/forward command"
+    it "command of forward"
+      export CURRENT=4 words=(__diraction-dispatch $DIR_DIR - l)
+      output="$(__diraction-dispatch)"
+      assert grep "$output" "_command_names"
+    end
+
+    it "argument of command with completion command available"
+      export CURRENT=5 words=(__diraction-dispatch $DIR_DIR - ls alo)
+      output="$(__diraction-dispatch)"
+      assert grep "$output" "_ls"
+    end
+
+    it "argument of command with no completion command available"
+      export CURRENT=5 words=(__diraction-dispatch $DIR_DIR - toto)
+      output="$(__diraction-dispatch)"
+      assert grep "$output" "_path_files -W $DIR_DIR"
+      assert grep "$output" "_options_set"
+    end
   end
 end
 rm -rf $DIR_DIR
